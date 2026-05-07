b4c300c6287a546cc1e0c572d330e9eb596a9b8c
Ali Rachidi <ali.rachidi73@gmail.com>
2026-05-07 09:09:36 -0400

    Prevent gaussian_notch filter when HEL is enabled

--- src/alpss/utils/validation.py
+++ src/alpss/utils/validation.py
@@ -111,3 +111,6 @@ def validate_inputs(inputs):
 
     if inputs["t_after"] > inputs["time_to_take"]:
         raise ValueError("'t_after' must be less than 'time_to_take'.")
+
+    if inputs["hel_calculation"] and inputs["carrier_filter_type"] == "gaussian_notch":
+        raise ValueError("HEL detection is incompatible with gaussian_notch filter (removes carrier signal needed for HEL analysis).")
